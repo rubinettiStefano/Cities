6634702703d88abc8f3486f82ea57436edb66231
rubinettiStefano <stefano.rubinetti@gmail.com>
2022-07-14 16:51:43 +0200

    14

--- Cities/src/main/java/com/generation/cities/controller/servlet/Context.java
+++ Cities/src/main/java/com/generation/cities/controller/servlet/Context.java
@@ -1,24 +1,33 @@
 package com.generation.cities.controller.servlet;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.generation.cities.model.database.Database;
-import com.generation.cities.model.database.MockDatabase;
+import com.generation.cities.model.database.DatabaseFactory;
 
 public class Context
 {
     
     private static List<Object> dependencies = new ArrayList<Object>();
     
+    
     static
     {
-	//Database database = DatabaseFactory.make("city.db");
-	Database database = new MockDatabase();
-	dependencies.add(database);
-	dependencies.add(new CityController(database));
-	dependencies.add(new BodyController(database));
-	dependencies.add(new CitizenController(database));
+	try
+	{
+           Database database = DatabaseFactory.make("C:\\Users\\rubin\\git\\Cities\\Cities\\src\\main\\webapp\\city.db");
+           dependencies.add(database);
+           dependencies.add(new CityController(database));
+           dependencies.add(new BodyController(database));
+           dependencies.add(new CitizenController(database));
+	}
+	catch(ClassNotFoundException | SQLException e )
+	{
+	    e.printStackTrace();
+	    System.exit(-1); // UCCIDI TUTTO. TERMINA PURE IL SERVER, SPEGNI TOMCAT
+	}
     }
     
     public static Object getDependency(String typeName)
